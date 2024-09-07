e463d2e46afda6ebe665162710d32bd7ff490335
sunghyun <20190171@inu.ac.kr>
2024-09-08 00:29:45 +0900

    feat : 주문/ 주문아이템 레포지토리 구현

---
+++ src/main/java/com/grepp/coffee_project/domain/order/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.grepp.coffee_project.domain.order.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+}
---
+++ src/main/java/com/grepp/coffee_project/domain/order/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.grepp.coffee_project.domain.order.repository;
+
+import com.grepp.coffee_project.domain.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+}
